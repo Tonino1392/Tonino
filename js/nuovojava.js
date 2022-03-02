c88bd22816a3ca2ce8d7527bc057552ad6370d22
Tonino1392 <antonio.campanale@outlook.it>
2022-03-02 13:01:42 +0100

    init repository

---
+++ js/nuovojava.js
@@ -0,0 +1,70 @@
+//Trova tutte le combinazioni di somma di un numero naturale
+/*var num;
+var i;
+
+num = prompt("Inserisci un numero naturale: ") * 1;
+
+document.write("<p>Le combinazioni sono le seguenti:</p>")
+
+for (i = 0; i <= num; i++) {
+    document.write("<p>" + i + " + " + num + "</p>");
+    num--
+} */
+
+//Conta le cifre di un numero naturale
+/*var num;
+
+num = prompt("Inserisci un numero naturale: ");
+
+var i = num.length;
+
+document.write(i);*/
+
+/*var numero1 = prompt("Inserisci numero uno: ") * 1;
+var numero2 = prompt("Inserisci numero due: ") * 1;
+var operatore = prompt("Inserisci l'operatore: + * - /");
+var risultato;
+
+switch (operatore) {
+
+    case '+':
+        risultato = (numero1 + numero2);
+        document.write("Somma: " + risultato);
+        break;
+    case '-':
+        risultato = numero1 - numero2;
+        document.write("Differenza: " + risultato);
+        break;
+    case '*':
+        risultato = numero1 * numero2;
+        document.write("Prodotto: " + risultato);
+        break;
+    case '/':
+        //  if (numero2 == 0) && (operatore == '/')
+        //{
+        alert("Non puoi dividere un numero con lo 0");
+        // }else {
+        risultato = numero1 / numero2;
+        document.write("Divisione: " + risultato);
+
+
+        break;
+        // }
+    default:
+        document.write("Non hai effettutato nulla");
+
+}*/
+
+/*function scambia() {
+    var a;
+    var b;
+
+    a = document.getElementById("Primo").innerHTML;
+    b = document.getElementById("Secondo").innerHTML;
+
+
+    document.getElementById("Primo").innerHTML = b;
+    document.getElementById("Secondo").innerHTML = a;
+
+
+}*/
